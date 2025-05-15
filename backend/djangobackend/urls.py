518c3e89f4f4e37aa27442ea5fd6a2ec76b31572
psyandrew <casero.andrew@gmail.com>
2025-05-15 16:44:31 +0800

    cloud deployment testing

--- backend/djangobackend/urls.py
+++ backend/djangobackend/urls.py
@@ -4,6 +4,6 @@ from dota_app.views import get_rosterdata, get_herodata, test
 
 urlpatterns = [
     path('roster', get_rosterdata), #for getting all data
-    path('hero/<str:hero_id>', get_herodata),#for getting specific data
+    path('hero/<str:hero_id>', get_herodata), #for getting specific data
     path('test', test)
 ]
